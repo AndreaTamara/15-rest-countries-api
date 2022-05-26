9d6194dd0c2e6dc5208a3e60406cf901a1af10a2
andrea <tamara11corra@gmail.com>
2022-05-26 01:04:29 -0500

    habilitada busqueda por pais

--- src/components/Countries/index.jsx
+++ src/components/Countries/index.jsx
@@ -1,28 +1,45 @@
-import { useEffect,useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect,useState} from 'react';
+import { Link,useLocation,useSearchParams } from 'react-router-dom';
 import { getData } from '../../apiFetch';
 import { CardCountry } from '../CardCountry';
 import { NavBar } from '../NavBar';
 import { Spin } from 'antd';
 import './Countries.css'
 
+
 export function Countries() {
 
   const [countries,SetCountries]= useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [searchParams, setSearchParams] = useSearchParams(useLocation().search);
+  const [filterParams, setfilterParams] = useSearchParams(useLocation().filter);
+  const searchedCountry = searchParams.get('search')
+  const searchedRegion = filterParams.get('filter')
+  console.log(searchedRegion)
+  
   
 
   useEffect(()=>{
-    setIsLoading(true)
-    getData('all')
+    
+    getData(searchedCountry?'name/'+searchedCountry:'all')
+    .then(data=>{
+      console.log(data)
+      SetCountries(data);
+      
+      setIsLoading(false)
+    } )
+   
+  },[searchedCountry])
+
+  useEffect(()=>{
+    
+    getData(searchedRegion?'continent/'+searchedRegion:'all')
     .then(data=>{
       SetCountries(data);
       setIsLoading(false)
-    }
-      
-      )
+    } )
    
-  },[])
+  },[searchedRegion])
 
   if (isLoading)  return <div className='spinner'> <Spin size="large" /></div> 
     return (
--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,22 +1,46 @@
 import { Input, Select } from 'antd';
+import { useSearchParams } from 'react-router-dom';
 import './NavBar.css'
 
 const { Option } = Select;
 
 
 export function NavBar() {
+    const [searchParams, setSearchParams] = useSearchParams();
 
+    const searchCountry = searchParams.get('search'||'')
+    const searchRegion = searchParams.get('filter'||'')
+
+    const handleInputChange = (e)=>{
+        const searched=e.target.value.toLowerCase()
+        if(searched){setSearchParams({search:searched})}
+        else{setSearchParams({search:''})}
+    }
+
+    const handleFilter = (value) => {
+        const filtered=value
+        if(filtered){setSearchParams({filter:filtered})}
+        else{setSearchParams({filter:''})}
+      };
+    
+      //console.log(searchCountry)
+      //console.log(searchRegion)
 
     return (
         <nav className='filters'>
-            <Input placeholder="Search for a country..." />
+            <Input 
+            placeholder="Search for a country..." 
+            value={searchCountry}
+            onChange={handleInputChange}/>
             <Select
-                showSearch
+                //showSearch
+                allowClear={true}
+                style={{width:'200px'}}
                 placeholder="Filter by Region"
                 optionFilterProp="children"
-                //onChange={onChange}
+                onChange={handleFilter}
                 //onSearch={onSearch}
-                filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
+                //filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}
             >
                 <Option value="africa">Africa</Option>
                 <Option value="america">America</Option>
